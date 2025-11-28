a1e2a90095839ecb0cf870047be27ae249f2f75c
garrisonglass <garrison.glass@gmail.com>
2025-11-28 11:39:16 -0500

    Added return 0

--- Ch 6 Programs.cpp
+++ Ch 6 Programs.cpp
@@ -2,10 +2,12 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
