11e913ab9b55b1c8c643136733b8814dca7063ac
garrisonglass <garrison.glass@gmail.com>
2025-12-02 21:13:30 -0500

    added comments

--- Ch 6 Programs.cpp
+++ Ch 6 Programs.cpp
@@ -14,7 +14,7 @@ void getCelsius()
 	cout << F << "F in Celsius is " << fixed << setprecision(2) << celsius(F) << endl;
 }
 
-double celsius(int F)
+double celsius(int F)//calculates the conversion 
 {
 	return (5.0 * (F - 32)) / 9;
 }
@@ -24,14 +24,14 @@ int main()
 	cout << "\nTable of Fahrenheit tempratures 0 - 20 and their Celcius equivalents.\n\n";
 	cout << "Fahrenheit\tCelcius\n\n";
 
-	for (int F = 0; F <= 20; F++)
+	for (int F = 0; F <= 20; F++)//Loops to creat the table 
 	{
 		cout << "\t" << setw(2) << F;
 		cout << "\t" << setw(3) << fixed << setprecision(2) << celsius(F) << endl;
 	}
 	cout << endl;
 
-	getCelsius();
+	getCelsius();//Callls the function to convert any F to C
 
 	return 0;
 }
