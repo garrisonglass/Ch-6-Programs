11d6fc3b16ecea5e8b24bda8e224a854698c8323
garrisonglass <garrison.glass@gmail.com>
2025-12-03 21:16:24 -0500

    Added comments

--- Ch 6 Programs.cpp
+++ Ch 6 Programs.cpp
@@ -4,9 +4,9 @@
 #include <iomanip>
 
 using namespace std;
-double celsius(int);
+double celsius(int);// Function prototype, Declares the celsius function so it can be used before its definition
 
-void getCelsius()
+void getCelsius()//Function: getCelsius, Prompts the user to enter a Fahrenheit temperature,then converts it to Celsius and displays the result.
 {
 	int F;
 	cout << "Enter temperature (F): ";
@@ -14,12 +14,12 @@ void getCelsius()
 	cout << F << "F in Celsius is " << fixed << setprecision(2) << celsius(F) << endl;
 }
 
-double celsius(int F)//calculates the conversion 
+double celsius(int F)// Function: celsius, Converts a Fahrenheit temperature to Celsius. Input: F (int) - temperature in Fahrenheit, Output: double - equivalent temperature in Celsius.Formula: (F - 32) * 5/9
 {
 	return (5.0 * (F - 32)) / 9;
 }
 
-int main()
+int main()//Displays a table of Fahrenheit temperatures from 0 to 20 and Celsius equivalents, then allows user input.
 {
 	cout << "\nTable of Fahrenheit tempratures 0 - 20 and their Celcius equivalents.\n\n";
 	cout << "Fahrenheit\tCelcius\n\n";
@@ -31,7 +31,7 @@ int main()
 	}
 	cout << endl;
 
-	getCelsius();//Callls the function to convert any F to C
+	getCelsius();// Call getCelsius() to let user convert any Fahrenheit value
 
 	return 0;
 }
